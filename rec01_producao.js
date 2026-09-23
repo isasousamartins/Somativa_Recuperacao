42f629899ecd92d5f55ba7b48546bd95301e7f74
Isabeli <isasmartins6@gmail.com>
2026-09-23 16:07:51 -0300

    exercicio1

---
+++ rec01_producao.js
@@ -0,0 +1,10 @@
+const quantidadeCaixas = 75;
+const horasTrabalhadas = 8;
+
+const producaoTotal = quantidadeCaixas * horasTrabalhadas;
+
+console.log("=== PRODUÇÃO DO TURNO ===");
+
+console.log(`Produção por hora: ${quantidadeCaixas} caixas`);
+console.log(`Horas do turno: ${horasTrabalhadas} horas`);
+console.log(`Produção total: ${producaoTotal} caixas"`);
