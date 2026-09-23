15fd998036b47c5291502702509412cdad854a4c
Isabeli <isasmartins6@gmail.com>
2026-09-23 16:15:33 -0300

    exercicio6

---
+++ rec06_media.js
@@ -0,0 +1,14 @@
+const entrada = require("readline-sync");
+
+let soma = 0;
+
+for (let i = 1; i <= 6; i++) {
+    const medicao = entrada.questionFloat(`Digite a medicao ${i}: `);
+    soma += medicao;
+}
+
+const media = soma / 6;
+
+console.log("\n=== RESULTADO DAS MEDICOES ===");
+console.log(`Soma das medicoes: ${soma}`);
+console.log(`Media final: ${media}`);
